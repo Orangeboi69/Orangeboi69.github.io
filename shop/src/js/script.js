48dc2e13a7e548bf22cac8b777762471ed42ce73
Orangeboi69 <121572232+Orangeboi69@users.noreply.github.com>
2023-12-08 14:46:42 +0100

    New Website
    None

---
+++ shop/src/js/script.js
@@ -0,0 +1,236 @@
+var g1 = 0;
+var g2 = 0;
+randomName = [
+    "Orange",
+    "Apple",
+    "Banana",
+    "Pineapple",
+    "Watermelon",
+    "Grape",
+    "Strawberry",
+    "Cherry",
+    "Mango",
+    "Pear",
+    "Lemon",
+    "Kiwi",
+    "Blueberry",
+    "Peach",
+    "Raspberry",
+    "Coconut",
+    "Lime",
+    "Grapefruit",
+    "Dragonfruit",
+    "Avocado",
+    "Blackberry",
+    "Pomegranate",
+    "Cantaloupe",
+    "Honeydew"
+]
+        function toggleMenu() {
+            const menuLinks = document.querySelector('.menu-links');
+            const bars = document.querySelectorAll('.bar');
+            const hamburgerMenu = document.querySelector('.hamburger-menu');
+            hamburgerMenu.classList.add('open');
+            menuLinks.classList.toggle('menu-open');
+            bars.forEach(bar => bar.classList.toggle('change'));
+            if (!menuLinks.classList.contains('menu-open')) {
+                hamburgerMenu.classList.remove('open');
+            }
+        } //Maybe use an evenlistener? and use variables?
+        function WebTitle() {
+            let now = new Date();
+            let month = now.getMonth();
+            let day = now.getDay();
+            var date = now.getDate();
+            const daterepresnts = {
+                0: "Sun",
+                1: "Mon",
+                2: "Tue",
+                3: "Wed",
+                4: "Thr",
+                5: "Fri",
+                6: "Sat"
+            };
+            const monthrepresents = {
+                0: "Jan",
+                1: "Feb",
+                2: "Mar",
+                3: "Apr",
+                4: "May",
+                5: "Jun",
+                6: "Jul",
+                7: "Aug",
+                8: "Sep",
+                9: "Oct",
+                10: "Nov",
+                11: "Dec"
+            };
+            if (date == 1) {
+                date = date + "st";
+            } else if (date == 2) {
+                date = date + "nd";
+            } else if (date == 3) {
+                date = date + "rd";
+            } else {
+                date = date + "th";
+            }
+            document.title = "Orange's Shop - " + daterepresnts[day] + "," + date;
+        }
+        function changeMode() {
+            let now = new Date();
+            let year = now.getFullYear();
+            let month = now.getMonth();
+            let day = now.getDay();
+            var date = now.getDate();
+            const daterepresnts = {
+                0: "Sunday",
+                1: "Monday",
+                2: "Tuesday",
+                3: "Wednesday",
+                4: "Thrusday",
+                5: "Friday",
+                6: "Saturday"
+            }
+            var date1 = daterepresnts[day];
+            let hour = now.getHours();
+            let minute = now.getMinutes();
+            let second = now.getSeconds();
+            let time = date1 + "," + year +"/"+ month +"/" + day +", at " + hour + ":" + minute + ":" + second;
+            console.log("Change mode function called " + time)
+                const body = document.querySelector('body');
+                const hamburgerMenu = document.querySelector('.hamburger-menu');
+                const content = document.querySelector('.content');
+                const hoverable = document.querySelectorAll('.hoverable');
+                
+                body.classList.toggle('dark-mode');
+                hamburgerMenu.classList.toggle('dark-mode');
+                content.classList.toggle('dark-mode');
+                hoverable.forEach(element => {
+                    element.classList.toggle('dark-mode');
+                });
+
+                // Additional logic for other elements if needed
+
+                const textblock = document.querySelector('.text-block');
+                textblock.classList.toggle('dark-mode');
+                // Change text for the button
+                const button = document.querySelector('.mode');
+                if (button.innerHTML === 'Dark mode') {
+                    button.innerHTML = 'Light mode';
+                } else {
+                    button.innerHTML = 'Dark mode';
+                    button.classList.toggle('dark-mode');
+                }
+                // for menu /-bar
+                const bars = document.querySelectorAll('.bar');
+
+                for (let j = 0; j < bars.length; j++) {
+                    bars[j].classList.toggle('dark-mode');
+                }
+            }
+        window.addEventListener('scroll', function() {
+            var hamburgerMenu = document.querySelector('.hamburger-menu');
+            var scrollPosition = window.scrollY;
+            if (scrollPosition > 0) {
+            // Add the 'scrolled' class to the hamburger menu when the user scrolls down
+            hamburgerMenu.classList.add('scrolled');
+            } else {
+            // Remove the 'scrolled' class from the hamburger menu when the user scrolls back to the top
+            hamburgerMenu.classList.remove('scrolled');
+            }
+        });
+        function TH() {
+            const element= document.querySelector('.trapezoid');
+            const B = document.querySelector('.trapezoid');
+            const BH = B.offsetHeight;  //BH - Border-Height(trapezoid)
+            const BW = B.offsetWidth/4; //BW - Border-Width(trapezoid)
+            const MBW = -BW;            //MBW - Minus Border-Width(trapezoid)
+            element.style.setProperty("--trapeH", BH + "px")
+            element.style.setProperty("--trapeW", BW + "px")
+            element.style.setProperty("--MtrapeW", MBW+0.25 + "px")
+            return BH, BW;
+        }
+        function game1(){
+            g1 += 1;
+            if (g1 == 5){
+                alert("You found a easter egg! (game1 on work)");
+                g1 = 0;
+                g2++;
+            } else if (g2 == 5){
+                alert("Stop it!")
+            }
+        }
+        function getULELEAmount(ListName){
+            var ulElement = document.querySelector(`${ListName}`); // 或者 'dl'
+            var liElements = ulElement.querySelectorAll('li');
+            return liElements.length;
+        }
+        function ItemsAlign(){ //Temporary function, working on one that can be used anywhere.
+            let lamount = getULELEAmount(".top-items");
+            if (window.innerWidth > window.innerHeight){
+                let llength = document.querySelector(".top-items").offsetWidth
+                let lwidth = llength / lamount - 0.1;
+                for (let i = 0; i < lamount; i++){
+                    document.querySelector(`.item-${i+1}`).style.width = lwidth + "px";
+                    document.querySelector(`.item-${i+1} .s-imgC`).style.height = (lwidth-0.1) + "px";
+                    document.querySelector(`.item-${i+1} .s-imgC`).style.width = (lwidth-0.1) + "px";                    
+                    
+                }
+        }
+        }
+        function fetchItems(amount) {
+            console.log("fetching target: http://127.0.0.1:5500/shop/src/test/${index}/item.json")
+            console.log("start fetching items...");
+            //Default Variables
+            var defaultImg = "/shop/src/img/default.png";
+            function fetchItemSequentially(index) {
+                console.log("fetching item: " + index);
+                fetch(`/shop/src/test/${index}/item.json`)
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error(`HTTP error! status: ${response.status}`);
+                        }
+                        return response.json();
+                    })
+                    .then(my_JSON_object => {
+                        var sObjImg = document.querySelector(`.item-${index} .s-image`);
+                        var sObjN = document.querySelector(`.item-${index} .name`);
+                        var sObjP = document.querySelector(`.item-${index} .price`);
+                        var sObjBuy = document.querySelector(`.item-${index} .buy`);
+                        sObjImg.src = my_JSON_object.image;
+                        sObjN.textContent = my_JSON_object.name;
+                        if (my_JSON_object.name == "$random") {
+                            sObjN.textContent = randomName[Math.floor(Math.random() * randomName.length)];
+                        }
+                        sObjP.textContent = my_JSON_object.price + "$";
+                        sObjBuy.setAttribute("onclick", `window.location.href='${my_JSON_object.url}'`);
+                    })
+                    .catch(error => {
+                        console.log('Fetch error: ', error);
+                    });
+            }
+            for (let x = 0; x < amount; x++) {
+              fetchItemSequentially(x+1);
+            }
+          }
+        //TODO: make a function that changes item.json s data, so it's easiert to edit in server side.
+        function sleep(ms) {
+
+            return new Promise(resolve => setTimeout(resolve, ms));
+        }
+        async function main(){
+                while (true){
+                    TH();
+                    ItemsAlign();
+                    await sleep(100);
+                }
+            }
+
+        //Should work on a easter egg game
+
+        console.log("Website by Orange :)");
+        console.log("followings are the debug menus.");
+        fetchItems(5);
+        WebTitle();
+        ItemsAlign();
+        main()
